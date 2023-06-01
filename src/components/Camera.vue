67b763f0d007c90f56159e26efa384c76511d29a
Warissara <warissara.liw@gmail.com>
2023-06-01 11:51:30 +0700

    bind POST request with camera

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -8,8 +8,12 @@
     const is_taken = ref(false);
     const is_modal_opened = ref(false);
     let stream;
+
+    let imageFile;
     
     defineExpose({ is_modal_opened });
+
+    // ------- Camera -------
 
     watch(is_modal_opened, (newVal, oldVal) => {
         if (newVal === true) {
@@ -62,10 +66,8 @@
                 const context = canvas.value.getContext("2d");
                 context.drawImage(player_value, 0, 0, width, height);
 
-                const data = canvas.value.toDataURL("image/jpeg");
-                
-                const img = new Image();
-                img.src = data;
+                const imageData = canvas.value.toDataURL("image/png");
+                imageFile = dataURLtoFile(imageData, "image.png");
 
                 stopBtn_Util();
             }); 
@@ -85,6 +87,45 @@
         stopBtn_Util();
     }
 
+    // ------- POST Request -------
+
+    function dataURLtoFile(dataURL, filename) {
+        const arr = dataURL.split(",");
+        const mime = arr[0].match(/:(.*?);/)[1];
+        const bstr = atob(arr[1]);
+        let n = bstr.length;
+        const u8arr = new Uint8Array(n);
+        while (n--) {
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        return new File([u8arr], filename, { type: mime });
+    }
+
+    function postBtn_Util() {
+        navigator.geolocation.getCurrentPosition(addItem);
+        this.$emit("post-created");
+        cancel_Util();
+    }
+
+    const addItem = async (position) => {
+        console.log(position.coords);
+        // const img = document.getElementById("myfile").files[0];
+        
+        const formData = new FormData();
+        formData.append("owner", "Veeliw");
+        formData.append("latitude", position.coords.latitude);
+        formData.append("longitude", position.coords.longitude);
+        formData.append("image", imageFile);
+
+        const options = {
+            method: "POST",
+            body: formData
+        };
+        const response = await fetch(import.meta.env.VITE_BACKEND + "/posts/post", options)
+            .catch((error) => console.error(error));
+        console.log("Response:", await response.json());
+    }
+
 </script>
 
 <template>
@@ -97,7 +138,7 @@
             <div class="flex justify-center pt-5">
                 <div class="flex justify-end w-10/12">
                     <button v-if="is_taken" @click=playBtn_Util class="rounded-full bg-sky-400 px-3 py-1 text-white drop-shadow">Retake</button>
-                    <button v-if="is_taken" class="rounded-full bg-green-400 px-3 py-1 text-white ml-3 drop-shadow">Post</button>
+                    <button v-if="is_taken" @click=postBtn_Util class="rounded-full bg-green-400 px-3 py-1 text-white ml-3 drop-shadow">Post</button>
                     <button v-else @click=captureBtn_Util class="rounded-full bg-sky-400 px-3 py-1 text-white drop-shadow">Capture</button>
                     <button @click=cancel_Util class="rounded-full bg-gray-400 px-3 py-1 text-white ml-3 drop-shadow">Cancel</button>
                 </div>
